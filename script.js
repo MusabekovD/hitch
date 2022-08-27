f832f9e8e9aa3e2f3e2bf57bbe48a844dabb9562
Lochinbek  Abduvoitov <lochinbekabduvoitov8@gmail.com>
2022-08-27 08:34:45 +0500

    1920px

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ let bag = document.getElementById('bag');
 let user = document.getElementById('user');
 let burger = document.getElementById('burger');
 let opened = document.querySelector('.header .opened');
-
+let bg = document.querySelector('.header-content .bg');
 burger.addEventListener('click', ()=>{
   opened.classList.toggle('active');
-
 });
 
